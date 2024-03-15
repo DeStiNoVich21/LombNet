0b235d3bf83cc5bc9163b3fe909bc6b28c38a7c7
Flikorf <136967876+Flikorf@users.noreply.github.com>
2024-03-15 10:46:36 +0500

    Fatal eror

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,31 +1,47 @@
 import { useState, useEffect } from "react";
 import styles from "../Catalog/Catalog.module.css";
 import { Link } from "react-router-dom";
+import API_BASE_URL from "../../apiConfig";
 
 export default function Catalog() {
   const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("https://localhost:7211/api/Fuji/categories")
+    fetch(`${API_BASE_URL}/api/Fuji/categories`)
       .then((response) => response.json())
-      .then((data) => setCategories(data))
-      .catch((error) => console.error("Error fetching categories:", error));
+      .then((data) => {
+        setCategories(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching categories:", error);
+        setLoading(false);
+      });
   }, []);
 
   return (
-    <ul className={styles.catalog}>
-      {categories.map((category) => (
-        <Link to={`/${category.category}`} key={category.category}>
-          <li className={styles.item}>
-            <img
-              src={`https://localhost:7211/api/Fuji/getImage/${category.imageFileName}`}
-              alt={category.name}
-              className={styles.image}
-            />
-            {category.name}
-          </li>
-        </Link>
-      ))}
-    </ul>
+    <div className={styles.catalogContainer}>
+      {loading ? (
+        <p>Loading...</p>
+      ) : categories.length === 0 ? (
+        <p>No categories found.</p>
+      ) : (
+        <ul className={styles.catalog}>
+          {categories.map((category) => (
+            <Link to={`/${category.category}`} key={category.category}>
+              <li className={styles.item}>
+                <img
+                  src={`${API_BASE_URL}/api/Fuji/getImage/${category.imageFileName}`}
+                  alt={category.name}
+                  className={styles.image}
+                />
+                {category.name}
+              </li>
+            </Link>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 }

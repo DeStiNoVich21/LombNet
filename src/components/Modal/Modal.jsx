c169c5e1e8b17eae5f98244589f49e81711d70d3
MorozovTimofey <104879885+MorozovTimofey@users.noreply.github.com>
2024-03-20 07:06:40 +0500

    Modaal update

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,13 +3,21 @@ import styles from "./Modal.module.css";
 
 const Modal = ({ onClose, children }) => {
   return (
-    <div className={styles.modal} onClick={onClose}>
-      <div className={styles.modalСontent} onClick={(e) => e.stopPropagation()}>
-        <div className={styles.modalContentContainer}>{children}</div>
-        <span className={styles.closeButton} onClick={onClose}>
-          ×
-        </span>
+    <div>
+      {/* Modal content */}
+      <div className={styles.modal}>
+        <div
+          className={styles.modalContent}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div className={styles.modalContentContainer}>{children}</div>
+          <span className={styles.closeButton} onClick={onClose}>
+            ×
+          </span>
+        </div>
       </div>
+      {/* Modal overlay */}
+      <div className={styles.modalOverlay}></div>
     </div>
   );
 };
